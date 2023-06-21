951a304caf4070433293b3db68c9be5aa880de7a
ViktoriiaTarasevych <vikka1836@gmail.com>
2023-06-21 22:36:20 +0300

    Revert "Revert "Push project""
    This reverts commit be41cb5951ab6c8c09094034003c6c57947cd8bd.

---
+++ src/main/java/com/teamchallenge/online_store/controller/ProductController.java
@@ -0,0 +1,36 @@
+package com.teamchallenge.online_store.controller;
+
+import com.teamchallenge.online_store.model.Product;
+import com.teamchallenge.online_store.servise.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping("/api/products")
+    public ResponseEntity<String> addProduct(@RequestBody Product product) {
+        try {
+            // Виклик сервісного шару для додавання товару
+            productService.addProduct(product);
+            return ResponseEntity.ok("Product added successfully");
+        } catch (Exception e) {
+            // Обробка помилок, якщо виникла проблема з додаванням товару
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Failed to add product: " + e.getMessage());
+        }
+    }
+    @GetMapping("/api/products/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        // Логіка для отримання товару з бази даних або іншого джерела
+        Product product = productService.getProductById(id);
+        return product;
+    }
+}
