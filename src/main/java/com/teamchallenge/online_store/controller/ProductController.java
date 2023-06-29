31b1da3176b7dea525c9fad11b9beee8fa8255a1
ViktoriiaTarasevych <vikka1836@gmail.com>
2023-06-29 19:42:20 +0300

    add CrossOrigin

--- src/main/java/com/teamchallenge/online_store/controller/ProductController.java
+++ src/main/java/com/teamchallenge/online_store/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000, https://candle-shop-by-ninjas-team.vercel.app")
 @RestController
 public class ProductController {
 
