133e7fb04442dff8d527da751a5ce79ff1073c1c
ViktoriiaTarasevych <vikka1836@gmail.com>
2023-07-01 16:11:14 +0300

    add swagger

--- src/main/java/com/teamchallenge/online_store/controller/ProductController.java
+++ src/main/java/com/teamchallenge/online_store/controller/ProductController.java
@@ -1,13 +1,21 @@
 package com.teamchallenge.online_store.controller;
 
+import com.teamchallenge.online_store.model.PageModel;
 import com.teamchallenge.online_store.model.Product;
 import com.teamchallenge.online_store.servise.ProductService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 //@CrossOrigin(origins = "http://localhost:3000, https://candle-shop-by-ninjas-team.vercel.app")
 @RestController
+@Api(tags = "Product")
 public class ProductController {
 
     private final ProductService productService;
@@ -16,14 +24,15 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @GetMapping("/")
+    @GetMapping("/api/")
     public String hello (String hello) {
 
         return "Привіт";
     }
 
 
-    @PostMapping("/products")
+    @PostMapping("/api/products")
+    @ApiOperation("Add product")
     public ResponseEntity<String> addProduct(@RequestBody Product product) {
         try {
             // Виклик сервісного шару для додавання товару
@@ -34,12 +43,21 @@ public class ProductController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Не вдалося додати продукт: " + e.getMessage());
         }
-
     }
-    @GetMapping("/products/{id}")
+
+    @GetMapping("/api/products/{id}")
+    @ApiOperation("Get product by id")
     public Product getProductById(@PathVariable Long id) {
-        // Логіка для отримання товару з бази даних або іншого джерела
+        // Логіка для отримання товару з бази даних
         Product product = productService.getProductById(id);
         return product;
+    }
+
+    @GetMapping("/api/products")
+    @ApiOperation("Get all products")
+    public PageModel<Product> getAllProducts ( @RequestParam(defaultValue = "0") int page,
+                                               @RequestParam(defaultValue = "10") int size ) {
+        Pageable pageable = PageRequest.of(page, size);
+        return productService.getAllProducts(pageable);
     }
 }
