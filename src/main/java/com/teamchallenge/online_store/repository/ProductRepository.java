f46ffde2eb1214e1dcb559a491a5f585997f2d7a
ViktoriiaTarasevych <vikka1836@gmail.com>
2023-07-01 16:11:47 +0300

    add PageModel

--- src/main/java/com/teamchallenge/online_store/repository/ProductRepository.java
+++ src/main/java/com/teamchallenge/online_store/repository/ProductRepository.java
@@ -1,8 +1,11 @@
 package com.teamchallenge.online_store.repository;
 
 import com.teamchallenge.online_store.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findAllBy(Pageable pageable);
 
 }
--- src/main/java/com/teamchallenge/online_store/servise/ProductService.java
+++ src/main/java/com/teamchallenge/online_store/servise/ProductService.java
@@ -1,10 +1,12 @@
 package com.teamchallenge.online_store.servise;
 
+import com.teamchallenge.online_store.model.PageModel;
 import com.teamchallenge.online_store.model.Product;
 import com.teamchallenge.online_store.repository.ProductRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
@@ -20,9 +22,16 @@ public class ProductService {
         productRepository.save(product);
     }
 
-    public List<Product> getAllProducts() {
-        // Повернення всіх товарів
-        return productRepository.findAll();
+    public PageModel<Product> getAllProducts(Pageable pageable) {
+        Page<Product> page = productRepository.findAll(pageable);
+
+        PageModel<Product> pageModel = new PageModel<>();
+        pageModel.setContent(page.getContent());
+        pageModel.setPageNumber(page.getNumber());
+        pageModel.setPageSize(page.getSize());
+        pageModel.setTotalElement(page.getTotalElements());
+
+        return pageModel;
     }
 
     public Product getProductById(Long id) {
