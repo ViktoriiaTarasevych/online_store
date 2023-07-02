695db1bf12be574e2c2593eef66216cc5064c0bf
ViktoriiaTarasevych <vikka1836@gmail.com>
2023-07-02 21:56:56 +0300

    delete springfox-swagger2 , springfox-swagger-ui

--- src/main/java/com/teamchallenge/online_store/OnlineStoreApplication.java
+++ src/main/java/com/teamchallenge/online_store/OnlineStoreApplication.java
@@ -5,17 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
-
 
 @SpringBootApplication
-@EnableSwagger2WebMvc
 public class OnlineStoreApplication {
 
 	public static void main(String[] args) {
@@ -30,22 +21,5 @@ public class OnlineStoreApplication {
 				registry.addMapping("/**");
 			}
 		};
-	}
-
-	@Bean
-	public Docket api () {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.teamchallenge.online_store.controller"))
-				.paths(PathSelectors.any())
-				.build();
-	}
-
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder()
-				.title("Your API Documentation")
-				.description("Description of your API")
-				.version("1.0.0")
-				.build();
 	}
 }
