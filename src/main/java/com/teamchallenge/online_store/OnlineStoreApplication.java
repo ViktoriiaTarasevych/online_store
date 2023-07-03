6786008a0cdaa935af079916219a6208ab912ab9
ViktoriiaTarasevych <vikka1836@gmail.com>
2023-07-03 18:54:52 +0300

    add @OpenAPIDefinition

--- src/main/java/com/teamchallenge/online_store/OnlineStoreApplication.java
+++ src/main/java/com/teamchallenge/online_store/OnlineStoreApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class OnlineStoreApplication {
 
 	public static void main(String[] args) {
