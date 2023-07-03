a7dcef287af13c219ce8f59876052bb47a3e3377
ViktoriiaTarasevych <vikka1836@gmail.com>
2023-07-03 20:37:20 +0300

    add @OpenAPIDefinition in @SpringBootApplication

--- src/main/java/com/teamchallenge/online_store/OnlineStoreApplication.java
+++ src/main/java/com/teamchallenge/online_store/OnlineStoreApplication.java
@@ -1,5 +1,6 @@
 package com.teamchallenge.online_store;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class OnlineStoreApplication {
 
 	public static void main(String[] args) {
