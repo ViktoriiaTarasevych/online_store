344b7cd445a00dfdda7283c3ee114300e963071e
ViktoriiaTarasevych <vikka1836@gmail.com>
2023-07-05 21:19:57 +0300

    add in product categoryId and categoryName

--- src/main/java/com/teamchallenge/online_store/model/Product.java
+++ src/main/java/com/teamchallenge/online_store/model/Product.java
@@ -13,6 +13,10 @@ public class Product {
 
     private String name;
 
+    private Long categoryId;
+
+    private String categoryName;
+
     public Long getId() {
         return id;
     }
@@ -29,11 +33,30 @@ public class Product {
         this.name = name;
     }
 
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
     public Product() {
     }
 
-    public Product(Long id, String name) {
+    public Product(Long id, String name, Long categoryId, String categoryName) {
         this.id = id;
         this.name = name;
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
     }
 }
