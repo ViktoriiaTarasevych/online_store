89c7a18d9e07369724cc9116436a4cf9277c02de
ViktoriiaTarasevych <vikka1836@gmail.com>
2023-07-13 22:33:55 +0300

    add  category.setName(category.getName()); in addCategory

--- src/main/java/com/teamchallenge/online_store/controller/CategoryController.java
+++ src/main/java/com/teamchallenge/online_store/controller/CategoryController.java
@@ -29,6 +29,7 @@ public class CategoryController {
     @Operation(summary = "Add category")
     public ResponseEntity<String> addCategory(@RequestBody Category category) {
         try {
+            category.setName(category.getName());
             categoryService.addCategory(category);
             return ResponseEntity.ok("Категорію успішно додано");
         } catch (Exception e) {
