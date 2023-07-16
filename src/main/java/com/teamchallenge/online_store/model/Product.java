273c5cf1bfb17dc8c8492958c7452df45d395952
ViktoriiaTarasevych <vikka1836@gmail.com>
2023-07-16 20:14:52 +0300

    revert   @GeneratedValue(strategy = GenerationType.AUTO)

--- src/main/java/com/teamchallenge/online_store/model/Product.java
+++ src/main/java/com/teamchallenge/online_store/model/Product.java
@@ -2,10 +2,6 @@ package com.teamchallenge.online_store.model;
 
 import jakarta.persistence.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
 @Entity
 public class Product {
 
