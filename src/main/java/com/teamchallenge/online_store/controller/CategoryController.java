df258e93579828b6107432b827c7f5c1c3a1f331
ViktoriiaTarasevych <vikka1836@gmail.com>
2023-07-20 18:23:53 +0300

    delete import io.swagger.v3.oas.annotations.parameters.RequestBody;

--- src/main/java/com/teamchallenge/online_store/controller/CategoryController.java
+++ src/main/java/com/teamchallenge/online_store/controller/CategoryController.java
@@ -4,13 +4,11 @@ import com.teamchallenge.online_store.model.Category;
 import com.teamchallenge.online_store.model.Product;
 import com.teamchallenge.online_store.servise.CategoryService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Set;
