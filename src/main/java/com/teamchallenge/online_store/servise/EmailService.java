d6f0ef98d59b6c6008db1f73e877156dab5be23c
ViktoriiaTarasevych <vikka1836@gmail.com>
2023-08-24 19:04:16 +0300

    changes in sendEmail method

--- src/main/java/com/teamchallenge/online_store/servise/EmailService.java
+++ src/main/java/com/teamchallenge/online_store/servise/EmailService.java
@@ -1,20 +1,15 @@
 package com.teamchallenge.online_store.servise;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mailjet.client.ClientOptions;
 import com.mailjet.client.MailjetClient;
 import com.mailjet.client.MailjetRequest;
 import com.mailjet.client.MailjetResponse;
 import com.mailjet.client.errors.MailjetException;
-import com.mailjet.client.resource.Email;
 import com.mailjet.client.resource.Emailv31;
-import com.mailjet.client.resource.Message;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 public class EmailService {
@@ -34,19 +29,27 @@ public class EmailService {
 
 
     public void sendEmail(String toEmail, String text) throws MailjetException {
-        MailjetRequest request = new MailjetRequest(Email.resource)
-                .property(Emailv31.MESSAGES, new JSONArray()
-                        .put(new JSONObject()
-                                .put(Emailv31.Message.FROM, new JSONObject()
-                                        .put("Email", "pidpal_official@ukr.net")
-                                        .put("Name", "PIDPAL"))
-                                .put(Emailv31.Message.TO, new JSONArray()
-                                        .put(new JSONObject()
-                                                .put("Email", toEmail)
-                                                .put("Name", "Recipient Name")))
-                                .put(Emailv31.Message.TEXTPART, text)));
+        try {
+            MailjetRequest request = new MailjetRequest(Emailv31.resource)
+                    .property(Emailv31.MESSAGES, new JSONArray()
+                            .put(new JSONObject()
+                                    .put(Emailv31.Message.FROM, new JSONObject()
+                                            .put("Email", "pidpal_official@ukr.net")
+                                            .put("Name", "PIDPAL"))
+                                    .put(Emailv31.Message.TO, new JSONArray()
+                                            .put(new JSONObject()
+                                                    .put("Email", toEmail)
+                                                    .put("Name", "Recipient Name")))
+                                    .put(Emailv31.Message.TEXTPART, text)));
 
-        MailjetResponse response = client.post(request);
+            MailjetResponse response = client.post(request);
+            if (response.getStatus() != 200) {
+                System.err.println("Error sending email. Mailjet response: " + response.getData());
+            }
+        } catch (MailjetException e) {
+            System.err.println("Mailjet error: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
 
