0c2912a9127eccff4b35cfc1e9379af28ea974a2
ViktoriiaTarasevych <vikka1836@gmail.com>
2023-09-17 21:24:24 +0300

    delete User with vsc

--- src/main/java/com/teamchallenge/online_store/controller/UserController.java
+++ src/main/java/com/teamchallenge/online_store/controller/UserController.java
@@ -1,87 +1,86 @@
-package com.teamchallenge.online_store.controller;
-
-import com.teamchallenge.online_store.model.User;
-import com.teamchallenge.online_store.servise.UserService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.NoSuchElementException;
-
-@RestController
-@RequestMapping("/api/user")
-@Tag(name = "User")
-public class UserController {
-
-    private final UserService userService;
-    private final AuthenticationManager authenticationManager;
-
-    public UserController(UserService userService, AuthenticationManager authenticationManager) {
-        this.userService = userService;
-        this.authenticationManager = authenticationManager;
-    }
-
-    @PostMapping("/register")
-    @Operation(summary = "Register a new user")
-    public ResponseEntity<String> registerUser(@RequestBody User user) {
-        try {
-            userService.saveUser(user);
-            return ResponseEntity.ok("Користувача успішно зареєстровано");
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Не вдалося зареєструвати користувача: " + e.getMessage());
-        }
-    }
-
-    @PostMapping("/login")
-    @Operation(summary = "Log in as a user")
-    public ResponseEntity<String> loginUser(@RequestBody User loginRequest) {
-        try {
-            authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword())
-            );
-            // Successful authentication
-            return ResponseEntity.ok("Успішний вхід в систему");
-        } catch (AuthenticationException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body("Помилка входу: невірні облікові дані");
-        }
-    }
-
-    @PutMapping("/{id}")
-    @Operation(summary = "Update user by ID")
-    public ResponseEntity<String> updateUserById(@PathVariable Long id, @RequestBody User updatedUser) {
-        try {
-            userService.updateUser(id, updatedUser);
-            return ResponseEntity.ok("Користувача оновлено");
-        } catch (NoSuchElementException e) {
-            return ResponseEntity.notFound().build();
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Не вдалося оновити користувача: " + e.getMessage());
-        }
-    }
-
-    @DeleteMapping("/{username}")
-    @Operation(summary = "Delete user by username")
-    public ResponseEntity<String> deleteUserByUsername(@PathVariable String username) {
-        try {
-            userService.deleteUser(username);
-            return ResponseEntity.ok("Користувача успішно видалено");
-        } catch (NoSuchElementException e) {
-            return ResponseEntity.notFound().build();
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Не вдалося видалити користувача: " + e.getMessage());
-        }
-    }
-
-}
+//package com.teamchallenge.online_store.controller;
+//
+//import com.teamchallenge.online_store.model.User;
+//import com.teamchallenge.online_store.servise.UserService;
+//import io.swagger.v3.oas.annotations.Operation;
+//import io.swagger.v3.oas.annotations.parameters.RequestBody;
+//import io.swagger.v3.oas.annotations.tags.Tag;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.NoSuchElementException;
+//
+//@RestController
+//@RequestMapping("/api/user")
+//@Tag(name = "User")
+//public class UserController {
+//
+//    private final UserService userService;
+//    private final AuthenticationManager authenticationManager;
+//
+//    public UserController(UserService userService, AuthenticationManager authenticationManager) {
+//        this.userService = userService;
+//        this.authenticationManager = authenticationManager;
+//    }
+//
+//    @PostMapping("/register")
+//    @Operation(summary = "Register a new user")
+//    public ResponseEntity<String> registerUser(@RequestBody User user) {
+//        try {
+//            userService.saveUser(user);
+//            return ResponseEntity.ok("Користувача успішно зареєстровано");
+//        } catch (Exception e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                    .body("Не вдалося зареєструвати користувача: " + e.getMessage());
+//        }
+//    }
+//
+//    @PostMapping("/login")
+//    @Operation(summary = "Log in as a user")
+//    public ResponseEntity<String> loginUser(@RequestBody User loginRequest) {
+//        try {
+//            authenticationManager.authenticate(
+//                    new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword())
+//            );
+//            // Successful authentication
+//            return ResponseEntity.ok("Успішний вхід в систему");
+//        } catch (AuthenticationException e) {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+//                    .body("Помилка входу: невірні облікові дані");
+//        }
+//    }
+//
+//    @PutMapping("/{id}")
+//    @Operation(summary = "Update user by ID")
+//    public ResponseEntity<String> updateUserById(@PathVariable Long id, @RequestBody User updatedUser) {
+//        try {
+//            userService.updateUser(id, updatedUser);
+//            return ResponseEntity.ok("Користувача оновлено");
+//        } catch (NoSuchElementException e) {
+//            return ResponseEntity.notFound().build();
+//        } catch (Exception e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                    .body("Не вдалося оновити користувача: " + e.getMessage());
+//        }
+//    }
+//
+//    @DeleteMapping("/{username}")
+//    @Operation(summary = "Delete user by username")
+//    public ResponseEntity<String> deleteUserByUsername(@PathVariable String username) {
+//        try {
+//            userService.deleteUser(username);
+//            return ResponseEntity.ok("Користувача успішно видалено");
+//        } catch (NoSuchElementException e) {
+//            return ResponseEntity.notFound().build();
+//        } catch (Exception e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                    .body("Не вдалося видалити користувача: " + e.getMessage());
+//        }
+//    }
+//
+//}
 
