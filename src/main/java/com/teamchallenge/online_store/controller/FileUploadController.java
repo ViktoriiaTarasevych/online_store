21a5696d61cbe8806ccd1a0e06011e0c25e19ebd
ViktoriiaTarasevych <vikka1836@gmail.com>
2023-09-17 21:13:42 +0300

    delete html with FileUploadController

--- src/main/java/com/teamchallenge/online_store/controller/FileUploadController.java
+++ src/main/java/com/teamchallenge/online_store/controller/FileUploadController.java
@@ -2,21 +2,22 @@ package com.teamchallenge.online_store.controller;
 
 import com.teamchallenge.online_store.errors.StorageFileNotFoundException;
 import com.teamchallenge.online_store.servise.StorageService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.stream.Collectors;
 
-@Controller
+@RestController
+@RequestMapping("/api")
+@Tag(name = "FileUpload")
 public class FileUploadController {
 
     private final StorageService storageService;
@@ -26,41 +27,34 @@ public class FileUploadController {
         this.storageService = storageService;
     }
 
-    @GetMapping("/")
-    public String listUploadedFiles(Model model) throws IOException {
-
-        model.addAttribute("files", storageService.loadAll().map(
-                        path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
-                                "serveFile", path.getFileName().toString()).build().toUri().toString())
-                .collect(Collectors.toList()));
-
-        return "uploadForm";
+    @GetMapping("/files")
+    public List<String> listUploadedFiles() throws IOException {
+        return storageService.loadAll()
+                .map(path -> path.getFileName().toString())
+                .collect(Collectors.toList());
     }
 
     @GetMapping("/files/{filename:.+}")
-    @ResponseBody
     public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
-
         Resource file = storageService.loadAsResource(filename);
-        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
+                .body(file);
     }
 
-    @PostMapping("/")
-    public String handleFileUpload(@RequestParam("file") MultipartFile file,
-                                   RedirectAttributes redirectAttributes) {
-
-        storageService.store(file);
-        redirectAttributes.addFlashAttribute("message",
-                "You successfully uploaded " + file.getOriginalFilename() + "!");
-
-        return "redirect:/";
+    @PostMapping("/upload")
+    public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) {
+        try {
+            storageService.store(file);
+            return ResponseEntity.ok("File uploaded successfully: " + file.getOriginalFilename());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Failed to upload file: " + file.getOriginalFilename());
+        }
     }
 
     @ExceptionHandler(StorageFileNotFoundException.class)
-    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<String> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("File not found");
     }
-
-
 }
